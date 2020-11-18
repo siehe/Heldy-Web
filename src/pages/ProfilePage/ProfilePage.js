9181d5dae68e8069df809a3b08c5a63f0774e089
Vitalii Bielievtsov <vitalii.bielievtsov@sigma.software>
2020-11-18 20:58:23 +0200

    [dev] added user prodile

--- src/pages/ProfilePage/ProfilePage.js
+++ src/pages/ProfilePage/ProfilePage.js
@@ -1,62 +1,81 @@
-import React, { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { getUserInfo } from '../../store/actions/getUserInfo';
+import React, { useEffect, useState } from 'react';
 import styles from './ProfilePage.module.scss';
 
 const ProfilePage = () => {
-    const dispacth = useDispatch();
-    const user = useSelector(store => store.userInfo);
+    const [userInfo, setUserInfo] = useState({});
+    const [ name, setName ] = useState('');
+    const [ surname, setSurname ] = useState('');
+    const [ email, setEmail ] = useState('');
+    const [ secondName, setSecondName ] = useState();
 
     useEffect(() => {
-        dispacth(getUserInfo());
+        fetch('https://heldy-api-pupi.azurewebsites.net/persons/' + localStorage.getItem('userId'), {
+            headers: {
+              "Content-Type": "application/json",
+              "Authorization": "Bearer " + localStorage.getItem('token'),
+            },
+          },).then(res => res.json()).then(data => 
+            {
+                setName(data);
+                setName(data.name);
+                setSecondName(data.secondName);
+                setEmail(data.email);
+                setSurname(data.surname);
+            }).catch(e => console.log(e.message));
     }, []);
 
-    return user && <div className={styles.wrapper}>
+    const submitEdir = e => {
+        e.preventDefault();
+
+        fetch()
+    }
+
+    return userInfo ? <div className={styles.wrapper}>
         <h2>Profile page</h2>
         <form>
             <div className={styles.row}>
                 <div className={styles.cell}>
                     <span>Name</span><br></br>
-                    <input type="text" name="name"/>
+                    <input type="text" name="name" value={name || ''} onChange={e => setName(e.target.value)}/>
                 </div>
                 <div>
                     <span>Surname</span><br></br>
-                    <input type="text" name="surname" />
+                    <input type="text" name="surname" value={surname || ''} onChange={e => setSurname(e.target.value)}/>
                 </div>
             </div>
             <div className={styles.row}>
                 <div className={styles.cell}>
-                    <span>Position</span><br></br>
-                    <input type="text" name="position" />
+                    <span>Second name</span><br></br>
+                    <input type="text" name="secondName" value={secondName || ''} onChange={e => setSecondName(e.target.value)}/>
                 </div>
                 <div>
                     <span>Department</span><br></br>
-                    <input type="text" name="department" />
+                    <input type="text" name="department"  disabled/>
                 </div>
             </div>
             <div className={styles.row}>
                 <div className={styles.cell}>
                     <span>E-mail</span><br></br>
-                    <input type="text" name="email" />
+                    <input type="text" name="email" value={email || ''} disabled/>
                 </div>
                 <div>
                     <span>Phone number</span><br></br>
-                    <input type="text" name="phoneNumber" />
+                    <input type="text" name="phoneNumber" disabled/>
                 </div>
             </div>
             <div className={styles.row}>
                 <div className={styles.cell}>
                     <span>Skype</span><br></br>
-                    <input type="text" name="skype" />
+                    <input type="text" name="skype" disabled/>
                 </div>
                 <div>
                     <span>Telegram</span><br></br>
-                    <input type="text" name="text" />
+                    <input type="text" name="text" disabled/>
                 </div>
             </div>
             <input type="submit" value="Підтвердити зміни" />
         </form>
-    </div>
+    </div> : null;
 }
 
 export default ProfilePage;
